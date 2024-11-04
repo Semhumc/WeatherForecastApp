b88e5ac17d363063e8f5e0920b6077a16d7e7036
Semhumc <semihumuc1@gmail.com>
2024-11-04 14:11:26 +0300

    add Viewpager

--- app/src/main/java/com/example/wheatherapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/wheatherapp/activity/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.wheatherapp.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.wheatherapp.R
+import com.example.wheatherapp.adapter.ViewPagerAdapter
+import com.example.wheatherapp.fragments.AnkaraFragment
+import com.example.wheatherapp.fragments.IstanbulFragment
+import com.example.wheatherapp.fragments.IzmirFragment
+import com.example.wheatherapp.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+            val fragments = listOf(MainFragment(), AnkaraFragment(), IstanbulFragment(), IzmirFragment()) // Replace with your fragment instances
+            val adapter = ViewPagerAdapter(fragments, this)
+            viewPager.adapter = adapter
         }
     }
-}
